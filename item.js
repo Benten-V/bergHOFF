3ecbcef6bab7c7e4334377329476f0127cfbe76b
KilianLeroy <153814835+KilianLeroy@users.noreply.github.com>
2024-04-29 21:58:18 +0200

    test1

--- item.js
+++ item.js
@@ -16,16 +16,22 @@
     }
     class Inventory{
         #items = [];
-        constructor(items = null) {
+        constructor(items) {
             this.#items = items;
+            this.fridgeupdate();
         }
         fridgeupdate(){
             const slotsList = document.querySelector(".list-frigo");
             const slots = slotsList.querySelectorAll(".frigo-item");
             let i = 0;
             this.#items.forEach(item => {
-
+                let itemElement = document.createElement("img");
+                itemElement.innerHTML = `src=${this.item.getImg()} alt=${this.item.getName()}`;
+                slots[i].appendChild(itemElement);
             });
         }
     }
+    const steak = new Item("steak", "./Images.fresh-red-meat-file-free-png.webp");
+    let inventoryList = [steak];
+    const fridge = new Inventory(inventoryList);
 })()
