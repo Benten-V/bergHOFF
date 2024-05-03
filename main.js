e65326d813c02822ae9a5a457b2f677a6dc6e18b
IrisDV <131678599+IrisDeVa@users.noreply.github.com>
2024-05-03 14:26:05 +0200

    diepvries
    Toevoegen klik op diepvries

--- main.js
+++ main.js
@@ -3,9 +3,11 @@
     const frigo = document.querySelector(".keuken-img-frigo");
     const keuken = document.querySelector(".main-keuken");
     const frigovenster = document.querySelector(".main-frigo");
-    const close = document.querySelector(".close");
+    const closeFrigo = document.querySelector(".close-frigo");
     const microgolf = document.querySelector(".keuken-img-micro");
-
+    const diepvries = document.querySelector(".keuken-img-diepvries");
+    const diepvriesvenster = document.querySelector(".main-diepvries");
+    const closeDiepvries = document.querySelector(".close-diepvries");
     const openItem = function (verborgen, zichtbaar) {
         verborgen.classList.add("hidden");
         zichtbaar.classList.remove("hidden");
@@ -15,6 +17,7 @@
         verborgen.classList.add("hidden");
     }
     frigo.addEventListener('click', () => openItem(keuken, frigovenster));
-    close.addEventListener('click', () => sluitItem(frigovenster, keuken));
-
+    closeFrigo.addEventListener('click', () => sluitItem(frigovenster, keuken));
+    diepvries.addEventListener('click', () => openItem(keuken, diepvriesvenster));
+    closeDiepvries.addEventListener('click', () => sluitItem(diepvriesvenster, keuken));
 })()
