0fe83b42b03ffd5f3bc0edf1096dde4c23b5a09c
IrisDV <131678599+IrisDeVa@users.noreply.github.com>
2024-05-03 13:14:12 +0200

    html/css/js update

--- main.js
+++ main.js
@@ -2,12 +2,19 @@
 (function() {
     const frigo = document.querySelector(".keuken-img-frigo");
     const keuken = document.querySelector(".main-keuken");
-    const frigovenster;
-    console.log(keuken);
-    const openFrigo = function () {
-        //Hier moet frigovenster getoond worden
-        keuken.classList.add("hidden");
+    const frigovenster = document.querySelector(".main-frigo");
+    const close = document.querySelector(".close");
+    const microgolf = document.querySelector(".keuken-img-micro");
 
+    const openItem = function (verborgen, zichtbaar) {
+        verborgen.classList.add("hidden");
+        zichtbaar.classList.remove("hidden");
     }
-    frigo.addEventListener('click', () => openFrigo());
+    const sluitItem = function (verborgen, zichtbaar) {
+        zichtbaar.classList.remove("hidden");
+        verborgen.classList.add("hidden");
+    }
+    frigo.addEventListener('click', () => openItem(keuken, frigovenster));
+    close.addEventListener('click', () => sluitItem(frigovenster, keuken));
+
 })()
