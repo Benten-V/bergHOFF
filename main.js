778a40aeface507790e6f96120b9f36544986d26
KilianLeroy <153814835+KilianLeroy@users.noreply.github.com>
2024-05-17 11:13:28 +0200

    kunt items uit frigo halen

--- main.js
+++ main.js
@@ -65,4 +65,64 @@ async function fetchLeaderboard() {
   terugkeuken.addEventListener("click", () => openItem(keukenzoom, keuken));
   kast.addEventListener("click", () => openItem(keukenzoom, kastkeuken));
   closekast.addEventListener("click", () => openItem(kastkeuken, keukenzoom));
+    class Item {
+        #name;
+        #img;
+        constructor(name, img) {
+            this.#name = name;
+            this.#img = img;
+        }
+        getName() {
+            return this.#name;
+        }
+        getImg() {
+            return this.#img;
+        }
+    }
+    class Inventory {
+        #items = [];
+        constructor(items) {
+            this.#items = items;
+            this.fridgeupdate();
+        }
+        fridgeupdate() {
+            const slotsList = document.querySelector(".list-inv");
+            const slots = slotsList.querySelectorAll(".inv-item");
+            let i = 0;
+            this.#items.forEach((item) => {
+                let itemElement = document.createElement("img");
+                itemElement.src = item.getImg();
+                itemElement.alt = item.getName();
+                slots[i].appendChild(itemElement);
+                itemElement.addEventListener("click",() => this.moveItem(this.#items[i], itemElement));
+                i++;
+            });
+        }
+        moveItem(item, itemElement){
+            sluitItem(frigovenster,keuken);
+            const groentenList = document.querySelector(".groenten");
+            const itemSlot = document.createElement("div");
+            groentenList.appendChild(itemSlot);
+            itemSlot.appendChild(itemElement);
+        }
+    }
+    const steak = new Item(
+        "steak",
+        "./Images/plastic-water-bottle-empty-e170N24-600-removebg-preview.png"
+    );
+    const steak2 = new Item(
+        "steak",
+        "./Images/plastic-water-bottle-empty-e170N24-600-removebg-preview.png"
+    );
+    const steak3 = new Item(
+        "steak",
+        "./Images/plastic-water-bottle-empty-e170N24-600-removebg-preview.png"
+    );
+    const steak4 = new Item(
+        "steak",
+        "./Images/plastic-water-bottle-empty-e170N24-600-removebg-preview.png"
+    );
+
+    let inventoryList = [steak, steak2, steak3, steak4];
+    const fridge = new Inventory(inventoryList);
 })();
