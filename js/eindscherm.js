7284149eb62d4e3c3401f928b8e00f27f002d2d5
Nathan <NathanHommez@gmail.com>
2024-05-25 13:37:01 +0200

    Class dampkap , js folder
    class dampkap volledig afgewerkt, alle js files in een js folder gestoke

--- js/eindscherm.js
+++ js/eindscherm.js
@@ -1,13 +1,10 @@
-import { punten } from "./punten.js";
+import { Punten } from "./punten.js";
 const buttonref = document.querySelector(".refresh");
 const buttonmin = document.querySelector(".minpunten");
 const totaalPunten = document.querySelector(".totaalPunten");
 
 buttonref.addEventListener("click", () => window.location.reload());
 
-const Punten = new punten();
-
 buttonmin.innerText = Punten.showMinPunten() + " Punten verloren";
 totaalPunten.innerText = Punten.showPunten() + " Punten gewonnen";
 // console.log(Punten.showMinPunten());
-
---
+++ js/punten.js
@@ -0,0 +1,31 @@
+class punten {
+  punten = 0;
+  minPuntenCount = 0;
+  Constructor() {
+    this.punten = 0;
+    this.minPuntenCount = 0;
+  }
+
+  plusPunten(punt) {
+    this.punten += punt;
+  }
+
+  minPunten(punt) {
+    this.punten -= punt;
+    this.minPuntencount += 1;
+  }
+
+  resetPunten() {
+    this.punten = 0;
+  }
+
+  showPunten() {
+    return this.punten;
+  }
+  showMinPunten() {
+    return this.minPuntencount;
+  }
+}
+
+const puntenInstance = new punten();
+export { puntenInstance as Punten };
