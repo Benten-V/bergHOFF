72b6223d7b20169f61def1b7d77590b1ba5f1990
Nathan <NathanHommez@gmail.com>
2024-05-28 14:11:58 +0200

    slaapkamer js
    volledige slaapkamer js

---
+++ js/slaapkamer.js
@@ -0,0 +1,42 @@
+import { Punten } from "./punten.js";
+
+const fig1 = document.querySelector(".fig-1");
+const fig2 = document.querySelector(".fig-2");
+const fig3 = document.querySelector(".fig-3");
+const fig4 = document.querySelector(".fig-4");
+
+let count1 = true;
+let count2 = true;
+
+fig1.addEventListener("click", () => {
+  if (count1) {
+    console.log("1");
+    Punten.plusPunten(2);
+    fig2.classList.add("hidden");
+    count1 = false;
+  }
+});
+fig2.addEventListener("click", () => {
+  if (count1) {
+    console.log("2");
+    Punten.minPunten(2);
+    fig1.classList.add("hidden");
+    count1 = false;
+  }
+});
+fig3.addEventListener("click", () => {
+  if (count2) {
+    console.log("3");
+    Punten.plusPunten(2);
+    fig4.classList.add("hidden");
+    count2 = false;
+  }
+});
+fig4.addEventListener("click", () => {
+  if (count2) {
+    console.log("4");
+    Punten.minPunten(2);
+    fig3.classList.add("hidden");
+    count2 = false;
+  }
+});
