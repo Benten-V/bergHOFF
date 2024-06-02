740cbdc4f5c88000e84813e5d33127890363d08f
IrisDV <131678599+IrisDeVa@users.noreply.github.com>
2024-06-02 12:04:59 +0200

    iets van functie uitgewerkt van controleren welk voedsel op plank en of het mag snijden

--- js/messen.js
+++ js/messen.js
@@ -3,6 +3,10 @@ import {koksmes} from "./elementen";
 
 const producten = inventoryList1;
 console.log(producten);
+/*
+In de functie checkVoedselTafel wordt gekeken welk voedsel op de plank ligt.
+Dit voedsel wordt omgezet tot het bestaande element en is ook het resultaat van deze functie.
+ */
 const checkVoedselTafel = function (){
     const snijplankFigure = document.querySelector(".snijplankplek");
     const element = snijplankFigure.querySelectorAll('img')[1];
@@ -13,12 +17,28 @@ const checkVoedselTafel = function (){
         }
     })
 }
-const snijd = function (mes) {
-    checkVoedselTafel();
-    console.log(checkVoedselTafel());
-    return true;
+/*
+In de functie magSnijden wordt gecontroleerd of het product dat op de plank ligt gesneden mag worden met het mes dat geselecteerd wordt.
+Deze functie geeft true of false terug.
+ */
+const magSnijden = function (mes) {
+    checkVoedselTafel()
+        .then(product => {
+            if (product.key('mes')[0] === mes){
+                return true;
+            }
+            else if (product.key('mes')[1]){
+                if (product.key('mes')[1] === mes){
+                    return true;
+                }
+            }
+            else {
+                console.log(mes, product.key('mes'));
+                return false;
+            }
+        })
 }
 
-koksmes.addEventListener('click', () => snijd("koksmes"))
+koksmes.addEventListener('click', () => magSnijden(koksmes))
 console.log(producten);
 console.log(checkVoedselTafel());
