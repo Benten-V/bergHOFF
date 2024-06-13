0e55ddedc3c800a73f505d9a3fc96dfcf90a48fa
IrisDV <131678599+IrisDeVa@users.noreply.github.com>
2024-06-13 11:00:02 +0200

    images voedsel universeel gemaakt

--- js/item.js
+++ js/item.js
@@ -178,39 +178,35 @@ class Kast extends Inventory {
 }
 const steak = new Vlees(
     "steak",
-    "./Images/fresh-red-meat-file-free-png.webp"
+    "./Images/vlees.png"
 );
 const kip = new Vlees(
     "kip",
-    "./Images/kisspng-roast-chicken-food-roasting-clip-art-chicken-roast-5b48ff6f35ccd8.0481672615315106392204-removebg-preview.png"
+    "./Images/kip.png"
 );
 const bacon = new Vlees(
     "bacon",
-    "./Images/pngtree-cartoon-pork-decorative-illustration-image_1454021-removebg-preview.png"
+    "./Images/bacon.png"
 );
-//   const steak4 = new Vlees(
-//     "steak",
-//     "./Images/plastic-water-bottle-empty-e170N24-600-removebg-preview.png"
-//   );
 
 const sla = new Groenten(
     "sla",
-    "./Images/184341742-verse-groene-sla-geïsoleerd-op-wit-removebg-preview.png"
+    "./Images/sla.png"
 );
 
 const tomaat = new Groenten(
     "tomaat",
-    "./Images/8424339-tomaat-geisoleerde-enkele-eenvoudige-cartoon-illustratie-vector-removebg-preview.png"
+    "./Images/tomaat.png"
 );
 
 const komkommer = new Groenten(
     "komkommer",
-    "./Images/kisspng-pickled-cucumber-vegetable-melon-clip-art-cucumber-5abc06278bb8c3.2746227815222717835723-removebg-preview.png"
+    "./Images/komkommer.png"
 );
 
 const wortel = new Groenten(
     "wortel",
-    "./Images/carrot-illustration-with-leaf-png.webp"
+    "./Images/wortel.png"
 );
 const pan1 = new Item("pan", "./Images/pan_1.png");
 const pan2 = new Item("pan", "./Images/pan_2.png");
@@ -235,58 +231,9 @@ let inventoryList2 = [
     kookpot3,
 ];
 let vleeslist = [steak, kip, bacon];
-let groetenlist = [sla, tomaat, komkommer, wortel];
-const fridge = new Fridge(inventoryList1, groetenlist, vleeslist);
+let groentenlist = [sla, tomaat, komkommer, wortel];
+const fridge = new Fridge(inventoryList1, groentenlist, vleeslist);
 const closet = new Kast(inventoryList2);
 
-//   class itemrandomizer {
-//     lijst = [];
-//     vleeslist = [];
-//     groentenlist = [];
-//     item1;
-//     item2;
-//     item3;
-//     constructor(lijst, groentenlist, vleeslist) {
-//       this.lijst = lijst;
-//       this.groentenlist = groentenlist;
-//       this.vleeslist = vleeslist;
-//     }
-
-//     randomizer() {
-//       this.item1 = Math.round(Math.random() * (this.vleeslist.length - 1) + 1);
-//       this.item2 = Math.round(
-//         Math.random() * (this.groentenlist.length - 1) + 1
-//       );
-//       let x = Math.round(Math.random() * (this.groentenlist.length - 1) + 1);
-//       if (this.item2 === x && x >= 2) {
-//         this.item3 = x - 1;
-//       } else if (this.item2 === x) {
-//         this.item3 = x + 1;
-//       } else {
-//         this.item3 = x;
-//       }
-//       console.log(this.item1, this.item2, this.item3);
-//       return this.item1, this.item2, this.item3;
-//     }
-
-//     update() {
-//       const randomlijst = document.querySelector(".itemrandomlijst");
-//       const randomli1 = randomlijst.querySelector(".item1");
-//       const randomli2 = randomlijst.querySelector(".item2");
-//       const randomli3 = randomlijst.querySelector(".item3");
-//       randomli1.textContent = vleeslist[this.item1 - 1].getName();
-//       randomli2.textContent = groetenlist[this.item2 - 1].getName();
-//       randomli3.textContent = groetenlist[this.item3 - 1].getName();
-//     }
-//     test() {
-//       console.log(tester);
-//     }
-//   }
-
-//   const itemRandomizer = new itemrandomizer(
-//     inventoryList,
-//     groetenlist,
-//     vleeslist
-//   );
-//   itemRandomizer.randomizer();
-//   const itemscijfers = itemRandomizer.update();
+/*Exports*/
+export {vleeslist, groentenlist, inventoryList1};
