69e70595e8ccc55c6ec6420d1b5bec46ab7bcb6b
IrisDV <131678599+IrisDeVa@users.noreply.github.com>
2024-06-13 11:52:06 +0200

    Snijden bijna compleet functies toegevoegd: zetProductNaarItem checkVoedingVanMes magSnijden zetGesnedenImg snijd zetVoedingOpPlank

--- js/messen.js
+++ js/messen.js
@@ -1,45 +1,117 @@
-import {inventoryList1} from "./item";
-import {koksmes} from "./elementen";
+import {vleeslist, groentenlist, inventoryList1} from "./item.js";
+import {koksmes, groentenmes, groentenplek} from "./elementen.js";
 
 const producten = inventoryList1;
-console.log(producten);
-/*
-In de functie checkVoedselTafel wordt gekeken welk voedsel op de plank ligt.
-Dit voedsel wordt omgezet tot het bestaande element en is ook het resultaat van deze functie.
- */
-const checkVoedselTafel = function (){
-    const snijplankFigure = document.querySelector(".snijplankplek");
-    const element = snijplankFigure.querySelectorAll('img')[1];
-    const voedselOpPlank = element.alt;
-    producten.forEach((product) => {
-        if (product.toString() === voedselOpPlank.toString()){
-            return product;
+
+/*Met deze functie kan een product naar een item van item.js omgezet worden*/
+const zetProductNaarItem = function (product) {
+    producten.forEach((item) => {
+        if (item.toString() === product) {
+            return item;
         }
     })
 }
 /*
-In de functie magSnijden wordt gecontroleerd of het product dat op de plank ligt gesneden mag worden met het mes dat geselecteerd wordt.
-Deze functie geeft true of false terug.
+In de functie checkVoedselTafel wordt gekeken welk voedsel op de plank ligt.
+Dit voedsel wordt omgezet tot het bestaande element en is ook het resultaat van deze functie.
  */
+const checkVoedselTafel = function () {
+    const snijplankFigure = document.querySelector(".snijplankplek");
+    const element = snijplankFigure.querySelectorAll('img')[1];
+    const voedselOpPlank = element.alt;
+    producten.forEach((product) => {
+        if (product.toString() === voedselOpPlank.toString()) {
+            return product;
+        }
+    })
+}
+/*In deze functie wordt voor elk mes een lijst gemaakt van voedsel dat met het mes gesneden mag worden.*/
+const checkVoedingVanMes = function (mes) {
+    let aanvaardeVoeding = []
+    switch (mes.toString()) {
+        case 'koksmes':
+            aanvaardeVoeding = +vleeslist;
+            aanvaardeVoeding = +groentenlist;
+            break;
+        case 'groentenmes':
+            aanvaardeVoeding = +groentenlist;
+            break;
+        default:
+            break;
+    }
+    return aanvaardeVoeding;
+}
+/*Hier wordt gecontroleerd of het mes het voedsel mag snijden*/
 const magSnijden = function (mes) {
-    checkVoedselTafel()
-        .then(product => {
-            if (product.key('mes')[0] === mes){
-                return true;
-            }
-            else if (product.key('mes')[1]){
-                if (product.key('mes')[1] === mes){
-                    return true;
-                }
-            }
-            else {
-                console.log(mes, product.key('mes'));
-                return false;
-            }
-        })
-        .catch(err => console.error(err));
+    const voeding = checkVoedselTafel();
+    const voedinglijst = checkVoedingVanMes(mes);
+    voedinglijst.forEach(voedsel => {
+        if (voedsel === voeding) {
+            return true;
+        }
+    })
+    return false;
+}
+/*Hier wordt de afbeelding van het element veranderd.*/
+const zetGesnedenImg = function (voedsel) {
+    let imglink = '../Images/';
+    switch (voedsel.toString()) {
+        case 'tomaat':
+            imglink = +'gesneden_tomaat.png';
+            break;
+        case 'sla':
+            imglink = +'gesneden_sla.png';
+            break;
+        case 'wortel':
+            imglink = +'gesneden_wortel.png';
+            break;
+        case 'komkommer':
+            imglink = +'gesneden_komkommer.png';
+            break;
+        case 'steak':
+            imglink = +'gesneden_steak.png';
+            break;
+        case 'kip':
+            imglink = +'gesneden_kip.png';
+            break;
+        case 'bacon':
+            imglink = +'gesneden_bacon.png';
+            break;
+        default:
+            imglink = +'close.png';
+            break;
+    }
+    return imglink;
+}
+/*In deze functie wordt het voedsel gesneden*/
+const snijd = function () {
+    const voeding = checkVoedselTafel();
+    zetGesnedenImg(voeding);
+}
+/*Door het klikken op een ingrediënt wordt deze op de snijplank gezet*/
+const zetVoedingOpPlank = function (voeding, voedingItem) {
+    /*element toevoegen snijplank*/
+    const snijplankFigure = document.querySelector(".snijplankplek");
+    const element = document.createElement('img');
+    element.src = voedingItem['img'];
+    element.alt = voedingItem['name'];
+    snijplankFigure.appendChild(element);
+   /*element verwijderen groentenplek*/
+    voeding.remove();
 }
 
-koksmes.addEventListener('click', () => magSnijden(koksmes))
-console.log(producten);
-console.log(checkVoedselTafel());
+koksmes.addEventListener('click', () => {
+    if (magSnijden(koksmes)) {
+        snijd();
+    } else {
+        //hier moet boodschap verschijnen dat dat niet mag
+    }
+})
+groentenmes.addEventListener('click', () => {
+    if (magSnijden(groentenmes)) {
+        snijd();
+    } else {
+        //hier moet boodschap verschijnen dat dat niet mag
+    }
+})
+//eventListeners moeten toegevoegd worden wanneer de elementen op de groentenplek worden gezet
